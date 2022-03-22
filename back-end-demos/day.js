3486fefc34dfac32a4d9a40798b99fd0e19c3bb5
dannydorazio <danny@dorazio.me>
2022-03-22 17:59:35 -0500

    Add back end demos from demonstration

---
+++ back-end-demos/day.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const axios = require('axios')
+
+const startDate = '2021-03-16'
+const endDate = '2022-03-16'
+
+function getAggregateBars(ticker, start, end, limit, timespan, multiplier) {
+    // const url = `https://polygon.io/v2/historic/${multiplier}/${timespan}/${symbol}?from=${start}&to=${end}&limit=${limit}`
+    const url = `https://api.polygon.io/v2/aggs/ticker/${ticker}/range/${multiplier}/${timespan}/${start}/${end}?adjusted=true&sort=asc&limit=${limit}&apiKey=${process.env.API_KEY}`
+    return axios.get(url)
+}
+
+function printAggregateBars(ticker, start, end) {
+
+    const limit = 50000
+    const timespan = 'day'
+    const multiplier = '1'
+
+    getAggregateBars(ticker, start, end, limit, timespan, multiplier)
+        .then(response => {
+            for (let i = 0; i < response.data.results.length; i++) {
+                const bar = response.data.results[i];
+                const date = new Date(bar.t);
+                console.log(`${date}, ${bar.c}`);
+            }
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
+
+printAggregateBars('AAPL', startDate, endDate);
