211cf953d4596acbffd110205e0e7b74def4b6cf
dannydorazio <danny@dorazio.me>
2022-03-26 12:33:32 -0400

    Add base vue files

---
+++ vue-daytrading-main/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <top-header />
+  <h1>Hello World</h1>
+</template>
+
+<script>
+import topHeader from "./components/topHeader.vue";
+
+export default {
+  components: { topHeader },
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ vue-daytrading-main/src/components/topHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="navbar">
+    <a>Tech Trading</a>
+    <a href="#">Account</a>
+    <a href="#">Trade</a>
+    <a href="#">Curated</a>
+    <a href="#">Logout</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "topHeader",
+};
+</script>
+
+<style scoped>
+/* header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+} */
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+  background-color: #333;
+}
+.navbar a {
+  color: #fff;
+  text-decoration: none;
+  padding: 10px;
+}
+</style>
