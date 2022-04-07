2c7ccb77f700ed4982a50423d92bff0a2dfb0339
Danny Dorazio <drd75@uakron.edu>
2022-04-06 23:46:39 -0400

    Add .env file

--- back-end-demos/day.js
+++ back-end-demos/day.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+
 const axios = require('axios')
 
 const startDate = '2021-03-16'
@@ -6,7 +6,7 @@ const endDate = '2022-03-16'
 
 function getAggregateBars(ticker, start, end, limit, timespan, multiplier) {
     // const url = `https://polygon.io/v2/historic/${multiplier}/${timespan}/${symbol}?from=${start}&to=${end}&limit=${limit}`
-    const url = `https://api.polygon.io/v2/aggs/ticker/${ticker}/range/${multiplier}/${timespan}/${start}/${end}?adjusted=true&sort=asc&limit=${limit}&apiKey=${process.env.API_KEY}`
+    const url = `https://api.polygon.io/v2/aggs/ticker/${ticker}/range/${multiplier}/${timespan}/${start}/${end}?adjusted=true&sort=asc&limit=${limit}&apiKey=p4PcUz4rRWTyXBDS6QZJTw07VwMJWZFE`
     return axios.get(url)
 }
 
