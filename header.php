6cbf170dcbbd68c729ca8eec86faa9449c2f6254
Danny Dorazio <drd75@uakron.edu>
2022-04-23 20:58:04 -0400

    Add header.php

---
+++ header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+<div class="navbar">
+        <img src="/src/assets/logo.png" />
+        <h1>Tech Trading</h1>
+        <a href="#">Home</a> |
+        <a href="#">Trade</a> |
+        <a href="#">Curated</a> |
+        <a href="#">Leaderboard</a> |
+        <a href="#">Logout</a>
+    </div>
+</body>
+
+<style scoped>
+    /* header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      margin-bottom: 10px;
+    } */
+    .navbar {
+        display: flex;
+        justify-content: normal;
+        align-items: center;
+        margin-bottom: 24px;
+        margin-top: 24px;
+        margin-left: 24px;
+        margin-right: 24px;
+        background-color: #030e12;
+        border-radius: 15px;
+    }
+
+    .navbar a {
+        color: #fff;
+        text-decoration: none;
+        padding: 10px;
+    }
+
+    h1 {
+        color: #fcde67;
+        padding-right: 10px;
+        padding-left: 10px;
+    }
+
+    img {
+        padding-left: 10px;
+    }
+</style>
+
+</html>
