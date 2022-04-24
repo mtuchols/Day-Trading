d9108e3b6534a0ea1218c834e24b61b6c94e3c84
Danny Dorazio <drd75@uakron.edu>
2022-04-23 23:24:47 -0400

    Remove formatting from home.php

--- home.php
+++ home.php
@@ -1,40 +1,37 @@
+<?php
+session_start();
+include("connection.php");
+include("functions.php");
+$user_data = check_login($con);
+?>
+
+
 <!DOCTYPE html>
 <html>
 
 <head>
     <meta charset="UTF-8">
-    <?php include "header.php"; ?>
+    <?php
+    include "header.php";
+    ?>
+    <link rel="stylesheet" href="styles.css">
 </head>
 
 <body>
+    <div class="all">
+        <div class="home">
+            <h2>Welcome <?php $user_data['firstName'] ?>!</h2>
+            <p>Email: {{ email }}</p>
+            <p>Current Balance: {{ email }}</p>
+        </div>
+        <div class="home">
+            <h2>Account Statistics</h2>
+            <p>All time profit: {{ atp }}</p>
+            <p>Favorite Stock: {{ favistock }}</p>
+            <p>Number of Trades Made: {{ numtrades }}</p>
+            <p>Average Profit Per Trade: {{ ppt }}</p>
+        </div>
+    </div>
 </body>
-<style>
-    h1 {
-        color: #fcde67;
-    }
-
-    h2 {
-        color: #fcde67;
-    }
-
-    body {
-        width: 45%;
-        padding-left: 24px;
-        margin-left: 24px;
-        margin-right: 24px;
-        margin-bottom: 50px;
-        border-radius: 15px;
-        float: left;
-        align-content: left;
-        text-align: left;
-        background-color: #99c3ef;
-    }
-
-    .all {
-        display: flex;
-        justify-content: space-around;
-        margin-bottom: 10px;
-    }
-</style>
 
 </html>
