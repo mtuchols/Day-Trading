d0297646997428e1c09b251d9a222f3377f99c33
Danny Dorazio <drd75@uakron.edu>
2022-04-23 21:25:26 -0400

    Add home.php

---
+++ home.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <?php include "header.php"; ?>
+</head>
+
+<body>
+</body>
+<style>
+    h1 {
+        color: #fcde67;
+    }
+
+    h2 {
+        color: #fcde67;
+    }
+
+    body {
+        width: 45%;
+        padding-left: 24px;
+        margin-left: 24px;
+        margin-right: 24px;
+        margin-bottom: 50px;
+        border-radius: 15px;
+        float: left;
+        align-content: left;
+        text-align: left;
+        background-color: #99c3ef;
+    }
+
+    .all {
+        display: flex;
+        justify-content: space-around;
+        margin-bottom: 10px;
+    }
+</style>
+
+</html>
