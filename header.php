cfb9d6c927ffb34207518dcbab99b8a0dbc8206a
Danny Dorazio <drd75@uakron.edu>
2022-04-23 23:25:07 -0400

    Remove formatting from header.php

--- header.php
+++ header.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles.css">
 </head>
+
 <body>
-<div class="navbar">
+    <div class="navbar">
         <img src="/src/assets/logo.png" />
         <h1>Tech Trading</h1>
         <a href="#">Home</a> |
@@ -14,41 +17,5 @@
         <a href="#">Logout</a>
     </div>
 </body>
-
-<style scoped>
-    /* header {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      margin-bottom: 10px;
-    } */
-    .navbar {
-        display: flex;
-        justify-content: normal;
-        align-items: center;
-        margin-bottom: 24px;
-        margin-top: 24px;
-        margin-left: 24px;
-        margin-right: 24px;
-        background-color: #030e12;
-        border-radius: 15px;
-    }
-
-    .navbar a {
-        color: #fff;
-        text-decoration: none;
-        padding: 10px;
-    }
-
-    h1 {
-        color: #fcde67;
-        padding-right: 10px;
-        padding-left: 10px;
-    }
-
-    img {
-        padding-left: 10px;
-    }
-</style>
 
 </html>
