c09c29f7c0afee3889d1821c6d5470a2cd6d358d
dannydorazio <danny@dorazio.me>
2022-04-25 22:56:58 -0400

    Most up to date trade

--- trade.php
+++ trade.php
@@ -19,10 +19,16 @@ include("functions.php");
 <body>
     <div class="trade">
         <h2>Trade</h2>
-        <label for="ticker">Step 1: Select a Stock</label>
-        <input type="ticker" id="tickerText" name="ticker1" placeholder="AAPL">
-        <button v-on:click="hello">Get Info</button>
-        <span id="stockinfo"></span>
+        <label for="ticker">Step 1 - Select a stock: </label>
+        <br>
+        <input id="tickerText" name="ticker1" placeholder="AAPL" oninput="this.value = this.value.toUpperCase()">
+        <br><br>
+        <label for="ticker">Step 2 - Select the amount of money you would like the algorithm to use: </label>
+        <br>
+        <input id="wagerText" name="wager" placeholder="1000">
+        <br><br>
+        <button onclick=" getStockData(tickerText)">Preview Trade</button>
+
     </div>
 </body>
 
