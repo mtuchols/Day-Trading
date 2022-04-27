76efd9436a6d6f6be8e0e583f21c4c7268e35e01
mtuchols <56408401+mtuchols@users.noreply.github.com>
2022-04-26 21:45:41 -0400

    Add files for algorithm pages

---
+++ coinFlipping.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+include("connection.php");
+include("functions.php");
+
+$ticker = $_COOKIE["ticker"];
+$start = $_COOKIE["start"];
+$startMoney = $_COOKIE["startMoney"];
+$startReference = $_COOKIE["startReference"];
+$algorithm = $_COOKIE["algorithm"];
+
+$tradeDay = mysql_query("SELECT MAX(tradeDay) FROM dailyTransactionData WHERE user_id = '$_SESSION['user_id']'");
+
+$query2 = "insert into dailyTransactionData (user_id, eodtotal, algorithmUsed, sodtotal, tradeDay) values ('$firstName', '$startMoney', '$algorithm', '$startReference', '$tradeDay')";
+mysqli_query($con, $query2);
+
+?>
+
