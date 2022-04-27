6eb1828944b2ebfcc59c590da75537a00fffb5ed
mtuchols <56408401+mtuchols@users.noreply.github.com>
2022-04-26 21:48:35 -0400

    Add files via upload

--- coinFlipping.php
+++ coinFlipping.php
@@ -9,7 +9,8 @@ $startMoney = $_COOKIE["startMoney"];
 $startReference = $_COOKIE["startReference"];
 $algorithm = $_COOKIE["algorithm"];
 
-$tradeDay = mysql_query("SELECT MAX(tradeDay) FROM dailyTransactionData WHERE user_id = '$_SESSION['user_id']'");
+$tradeDay = mysql_query("SELECT MAX(tradeDay) FROM dailyTransactionData WHERE user_id = '$_SESSION['user_id']'"); 
+$tradeDay = $tradeDay +1;
 
 $query2 = "insert into dailyTransactionData (user_id, eodtotal, algorithmUsed, sodtotal, tradeDay) values ('$firstName', '$startMoney', '$algorithm', '$startReference', '$tradeDay')";
 mysqli_query($con, $query2);
